01d43dbfd57b1c05a778a5e7cc6885555d2e2ba0
SamiDev <71267011+samidevio@users.noreply.github.com>
2021-03-09 10:39:43 +0600

    Update update.js

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
-var currentVersion = 2.1;
+var currentVersion = 2.0;
 if(scriptVersion < currentVersion){
 	$('#adminContent').prepend('<div class="container-fluid mt-4"><div class="alert alert-primary mb-0" role="alert"><a target="__blank" href="https://www.codegrape.com/item/pikup-responsive-multiple-image-uploader-and-shorten-url/45643">PikUp v'+currentVersion+' - Update Now!</a></div></div>');
 }
